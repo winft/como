a27f4eac4e64b2c9da015142b268ecd71be15304
Roman Gilg <subdiff@gmail.com>
2024-02-09 20:50:33 +0100

    build: remove KCMs
    These are removed form the pure library part.

--- kcms/effects/Messages.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-# SPDX-FileCopyrightText: 2023 Roman Gilg <subdiff@gmail.com>
-#
-# SPDX-License-Identifier: GPL-2.0-or-later
-
-$XGETTEXT `find . -name \*.cpp -o -name \*.qml` -o $podir/kcm_kwin_effects.pot
