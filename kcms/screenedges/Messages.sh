baa4927d5a533965d32bcdbb99af340295916921
Roman Gilg <subdiff@gmail.com>
2024-02-09 20:50:33 +0100

    build: remove KCMs
    These are removed form the pure library part.

--- kcms/screenedges/Messages.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-# SPDX-FileCopyrightText: 2023 Roman Gilg <subdiff@gmail.com>
-#
-# SPDX-License-Identifier: GPL-2.0-or-later
-
-$EXTRACTRC *.ui >> rc.cpp || exit 11
-$XGETTEXT *.cpp -o $podir/kcmkwinscreenedges.pot
-rm -f rc.cpp
--- kcms/scripts/Messages.sh
+++
@@ -1,9 +0,0 @@
-#!bin/sh
-
-# SPDX-FileCopyrightText: 2023 Roman Gilg <subdiff@gmail.com>
-#
-# SPDX-License-Identifier: GPL-2.0-or-later
-
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/kcm_kwin_scripts.pot
-rm -f rc.cpp
