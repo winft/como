48e720608e9c8bd00c5e05ff0bd8c0241d2e65db
Roman Gilg <subdiff@gmail.com>
2024-02-26 20:28:17 +0100

    build: remove Messages.sh files
    They are not in use at the moment and outdated.

--- Messages.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-# SPDX-FileCopyrightText: 2023 Roman Gilg <subdiff@gmail.com>
-#
-# SPDX-License-Identifier: GPL-2.0-or-later
-
-$EXTRACTRC `find . -not -path "./kcms/*" \( -name \*.kcfg -o -name \*.ui \)` >> rc.cpp || exit 11
-$XGETTEXT `find . -not -path "./kcms/*" \( -name \*.cpp -o -name \*.qml \)` -o $podir/kwin.pot
-rm -f rc.cpp
