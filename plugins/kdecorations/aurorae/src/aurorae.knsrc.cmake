84b3a22e2d84a4f26ddc4603cfa895b1552fe595
Nate Graham <nate@kde.org>
2024-04-01 13:35:59 -0600

    feat: mark risky KNS content
    By design, these categories of content can run arbitrary code on the
    system. This makes them security-sensitive and risky, so we need to
    inform the user about it.
    
    Depends on https://invent.kde.org/frameworks/knewstuff/-/merge_requests/309

--- plugins/kdecorations/aurorae/src/aurorae.knsrc.cmake
+++ plugins/kdecorations/aurorae/src/aurorae.knsrc.cmake
@@ -43,6 +43,7 @@ Name[zh_CN]=Aurorae 窗口装饰
 Name[zh_TW]=Aurorae 視窗裝飾
 
 ProvidersUrl=https://autoconfig.kde.org/ocs/providers.xml
+ContentWarning=Executables
 Categories=Window Decoration Aurorae
 Uncompress=archive
 TargetDir=aurorae/themes
